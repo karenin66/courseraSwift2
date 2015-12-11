5096398864dc8cba03de550fc2c8183bbb057691
karenin66 <kareninplus@hotmail.com>
2015-12-11 20:30:02 +0100

    juegoDeMemoria.v1

---
+++ juegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for i in 0...100 {
+    if (i>=30 && i<=40){
+        print("\(i)\t" + "Viva Swift!!!")
+    } else if i%5==0 {
+        print("\(i)\t" + "Bingo!!!")
+    } else if i%2==0 {
+        print("\(i)\t" + "Par!!!")
+    } else {print("\(i)\t" + "Impar!!!")}
+}
